570c8fea10c2df36fed96e3bbd0fd8f0f25cafae
Timothy A. Perez <tim.adam.perez@gmail.com>
2013-05-16 10:12:41 -0400

    Added Benchmark Class

---
+++ inc/benchmark.php
@@ -0,0 +1,35 @@
+<?php
+
+		class Benchmark {
+      public $start,$end,$result;
+      public function __construct(){
+        $this->start();
+      }
+      public function start(){
+        $this->start = microtime(true);
+      }
+      public function end(){
+        $this->end = microtime(true);
+        $this->result = $this->end - $this->start;
+      }
+      public function getResult(){
+        return $this->result;
+      }
+    }
+
+/* 
+ * == EXAMPLE USAGE ==
+
+	// Top of Your PHP
+	$benchmark = new Benchmark();
+
+
+	// End of Your PHP
+	$benchmark->end();
+
+
+	// Return Result
+	echo $benchmark->getResult();
+
+
+*/
